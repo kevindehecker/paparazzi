54d94bb23770aab00b1831dcaa1e420bcdd0f009
kevindehecker <kevindehecker@hotmail.com>
2013-08-13 08:45:33 +0200

    tradiela

--- sw/ext/ardrone2_video/initvideoall.sh
+++ sw/ext/ardrone2_video/initvideoall.sh
@@ -1,7 +1,7 @@
 echo kevins video initializer!
 kill -9 `pidof program.elf`
 kill -9 `pidof gst-launch-0.10`
-mount --bind /data/video/kevin/opt /opt
+# mount --bind /data/video/kevin/opt /opt happens from ppz
 export PATH=/opt/arm/gst/bin:$PATH
 export DSP_PATH=/opt/arm/tidsp-binaries-23.i3.8/
 /bin/dspbridge/cexec.out -T /opt/arm/tidsp-binaries-23.i3.8/baseimage.dof -v
