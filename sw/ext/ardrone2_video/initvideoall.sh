0ec06d2c137931b2711dcd614e44b8be69e035ec
Kevin <kvanhecke@student.tudelft.nl>
2013-08-13 14:03:51 +0200

    messages are now fed correctly into ppz, working brightness example

--- sw/ext/ardrone2_video/initvideoall.sh
+++ sw/ext/ardrone2_video/initvideoall.sh
@@ -1,10 +1,10 @@
 echo kevins video initializer!
 kill -9 `pidof program.elf`
 kill -9 `pidof gst-launch-0.10`
-# mount --bind /data/video/kevin/opt /opt happens from ppz
+# mount --bind /data/video/raw/opt /opt happens from ppz
 export PATH=/opt/arm/gst/bin:$PATH
 export DSP_PATH=/opt/arm/tidsp-binaries-23.i3.8/
 /bin/dspbridge/cexec.out -T /opt/arm/tidsp-binaries-23.i3.8/baseimage.dof -v
 /bin/dspbridge/dynreg.out -r /opt/arm/tidsp-binaries-23.i3.8/m4venc_sn.dll64P -v
 
-gst-launch v4l2src device=/dev/video1 ! videorate ! 'video/x-raw-yuv,framerate=15/1' ! videoscale ! video/x-raw-yuv, width=544, height=304 ! dspmp4venc ! rtpmp4vpay config-interval=2 ! udpsink host=192.168.1.5 port=5000
+gst-launch v4l2src device=/dev/video1 ! videorate ! 'video/x-raw-yuv,framerate=15/1' ! videoscale ! video/x-raw-yuv, width=160, height=60 ! example tcp_port=2002 ! fakesink # dspmp4venc ! rtpmp4vpay config-interval=2 ! udpsink host=192.168.1.5 port=5000
