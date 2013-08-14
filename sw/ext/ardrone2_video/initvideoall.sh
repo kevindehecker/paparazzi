f7792fcc74704e92bed90765b0db712e1851caa8
kevindehecker <kevindehecker@hotmail.com>
2013-08-14 14:35:47 +0200

    Working video stream, example disabled. Framework auto uploaded.

--- sw/ext/ardrone2_video/initvideoall.sh
+++ sw/ext/ardrone2_video/initvideoall.sh
@@ -7,4 +7,4 @@ export DSP_PATH=/opt/arm/tidsp-binaries-23.i3.8/
 /bin/dspbridge/cexec.out -T /opt/arm/tidsp-binaries-23.i3.8/baseimage.dof -v
 /bin/dspbridge/dynreg.out -r /opt/arm/tidsp-binaries-23.i3.8/m4venc_sn.dll64P -v
 
-gst-launch v4l2src device=/dev/video1 ! videorate ! 'video/x-raw-yuv,framerate=15/1' ! videoscale ! video/x-raw-yuv, width=160, height=60 ! example tcp_port=2002 ! fakesink # dspmp4venc ! rtpmp4vpay config-interval=2 ! udpsink host=192.168.1.5 port=5000
+gst-launch v4l2src device=/dev/video1 ! videorate ! 'video/x-raw-yuv,framerate=15/1' ! videoscale ! video/x-raw-yuv, width=544, height=304 ! dspmp4venc ! rtpmp4vpay config-interval=2 ! udpsink host=192.168.1.4 port=5000
