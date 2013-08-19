69491824f67adc2ada6a4abc8d8b81a86ede6f15
Kevindehecker <kevindehecker@hotmail.com>
2013-08-19 09:12:08 +0200

    Serialized message struct used as communication between ppz and gst plugin, untested

---
+++ sw/airborne/subsystems/video/video_message_structs.h
@@ -0,0 +1,18 @@
+#ifndef VMSTRT_H
+#define VMSTRT_H
+
+struct gst2ppz_message_struct {
+  unsigned int maxY;		//maximum brightness
+  unsigned int max_idx;		//x coordinate of brightest pixel
+  unsigned int max_idy;		//y .....
+  unsigned int counter;		//counter to keep track of data
+};
+extern struct gst2ppz_message_struct gst2ppz;
+
+struct ppz2gst_message_struct {
+  unsigned int heading;
+};
+extern struct ppz2gst_message_struct ppz2gst;
+
+#endif  /*  VMSTRT_H  */
+
