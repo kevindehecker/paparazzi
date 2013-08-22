d6afb864c58db4e43e6f3c701711fccaa769dc43
Kevindehecker <kevin30kv@gmail.com>
2013-08-22 09:14:07 +0200

    working zbar framework, hacked fix for ip script

---
+++ sw/ext/ardrone2_video/generate_initvideoall.sh
@@ -0,0 +1,4 @@
+ip="$(ifconfig | grep  192.168.1 | cut -d ':' -f 2 |  cut -d ' ' -f 1)"
+echo $ip
+echo $1
+echo "gst-launch v4l2src device=/dev/video1 ! videorate ! 'video/x-raw-yuv,framerate=15/1' ! videoscale ! video/x-raw-yuv, width=544, height=304 ! example tcp_port=2002 ! fakesink #dspmp4venc ! rtpmp4vpay config-interval=2 ! udpsink host=$ip port=5000" >> $1/initvideoall.sh
