2cf51a966c031f2c232089fba859ea6af3905aa3
kevindehecker <kevindehecker@hotmail.com>
2014-11-05 11:26:38 +0100

    resetted IC code to exclude undeeded beep quad changes

--- sw/airborne/modules/AutoHeading/autoheading.c
+++ sw/airborne/modules/AutoHeading/autoheading.c
@@ -24,30 +24,23 @@
  *  @brief interface to usb stereocam 
  */
 
- #include "autoheading.h"
- #include "generated/modules.h"
-
+#include "autoheading.h"
+#include "generated/modules.h"
 #include <stdbool.h>
-
 #include "firmwares/rotorcraft/guidance/guidance_h.h" // to set heading
-
-// Serial Port
+#include "led.h"
 #include "mcu_periph/uart.h"
+#include "subsystems/datalink/downlink.h"
 
 #define STEREO_PORT    UART1
-
-
-#include "led.h"
-
-
-
 
 #define __StereoLink(dev, _x) dev##_x
 #define _StereoLink(dev, _x)  __StereoLink(dev, _x)
 #define StereoLink(_x) _StereoLink(STEREO_PORT, _x)
 #define StereoBuffer() StereoLink(ChAvailable())
 
-uint8_t vision_threshold;
+uint8_t vision_objectthreshold;
+uint8_t vision_colorthreshold;
 bool vision_turnbutton;
 float vision_turnspeed;
 float vision_pitchangle;
@@ -55,19 +48,43 @@ float vision_pitchangle;
 float vision_turnStepSize;
 uint32_t hysteresesDelay;
 
+uint8_t maxU;
+uint8_t minU;
+uint8_t maxV;
+uint8_t minV;
+uint8_t str[2] = {};
 
 extern void autoheading_turnButton(float whatever) {
     whatever = whatever;
     vision_turnbutton = true;
 }
 
+extern void autoheading_setMaxU(uint8_t value) {
+    // maxU = value;
+    
+    // unsigned char tmp[3];
+    // tmp[0] = 'U';
+    // tmp[1] = value;
+    // tmp[2] = '\0';
+
+    // StereoLink(Transmit(tmp));
+}
+
 extern void autoheading_start(void){
     vision_turnspeed = 1;
-    vision_threshold = 7;
+    vision_colorthreshold = 7;
+    vision_objectthreshold = 5;
     vision_turnbutton = false;
     vision_pitchangle=-4;
     hysteresesDelay=0;
     vision_turnStepSize=120;
+
+    maxU = 66;
+    minU = 66;
+    maxV = 66;
+    minV = 66;
+
+
     UART1Init();
 }
 
@@ -76,48 +93,51 @@ extern void autoheading_stop(void) {
 
 }
 
-
 static void stereo_parse(uint8_t c);
 static void stereo_parse(uint8_t c) {
-    if (hysteresesDelay==0) {
-        if (c < (vision_threshold + '0' )) {  
-            hysteresesDelay = (vision_turnStepSize / vision_turnspeed ) / (float)AUTOHEADING_PERIODIC_FREQ;
-            incrementHeading(vision_turnStepSize);
-           // LED_ON(3); //RADIO_CONTROL_LED
-           // LED_ON(2); 
-            LED_ON(1); 
-        } else {
-           // LED_TOGGLE(3);
-           // LED_TOGGLE(2);
-            LED_TOGGLE(1);
-
-        } 
+    
+    /* debug message */
+    if (c > 127) {
+        str[1] = c; // roof/color
     }
+    else {
+        str[0] = c; // objects
+    }
+    DOWNLINK_SEND_DEBUG(DefaultChannel, DefaultDevice, 2, str);
 
+    if (hysteresesDelay==0) {
+        
+        if (c > 127) { // roof detector byte
+            if (c < (vision_colorthreshold -127)) {  
+                hysteresesDelay = (vision_turnStepSize / vision_turnspeed ) / (float)AUTOHEADING_PERIODIC_FREQ;
+                incrementHeading(vision_turnStepSize);           
+                LED_ON(1);                                
+            } 
+        } else { // stereo detector byte
+            if (c < vision_objectthreshold) {  
+                hysteresesDelay = (vision_turnStepSize / vision_turnspeed ) / (float)AUTOHEADING_PERIODIC_FREQ;
+                incrementHeading(vision_turnStepSize);                    
+                LED_ON(1); 
+            } 
+        }        
+    } else {
+        LED_OFF(1); //off if nothing is happening
+    }
+    
 }
 
 extern void autoheading_periodic(void) {
+    
+    stereo_parse(StereoLink(Getch()));               
+    if (UART1ChAvailable())
+    {
+      while (StereoLink(ChAvailable()))
+        stereo_parse(StereoLink(Getch()));
+    }
+   
     if (hysteresesDelay>0) {
         hysteresesDelay--;        
     }
-    
-
-    stereo_parse(StereoLink(Getch()));
-
-        
-        
-  if (UART1ChAvailable())
-  {
-
-    
-
-
-    while (StereoLink(ChAvailable()))
-      stereo_parse(StereoLink(Getch()));
-  }
-
-    
-
     setHeading_P(vision_turnspeed);
     setAutoHeadingPitchAngle(vision_pitchangle);
 
