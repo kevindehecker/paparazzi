c42f2c2728fb7b7efe51a88ddac3a61f337ad918
kevindehecker <kevindehecker@hotmail.com>
2017-09-24 15:36:11 +0200

    Upgrade QTC version, add more QTCreator compatibility with chibios

--- sw/tools/find_vpaths.py
+++ sw/tools/find_vpaths.py
@@ -30,7 +30,67 @@ for i in range(2,len(sys.argv)):
 
 #create the command for gcc -MM cflags srcs
 cmd = cc + ' -MM '
-for flg in cflags:
+for flg in cflags: 
+    #remove some flags that appeared here for unknown reasons since last time I made this
+    id1 =flg.find("-MD -MP -MF")
+    if (id1 > -1):        
+        id2 =flg.find("!") 
+        flg = flg[0:id1] + flg[id2+1:]
+
+    #append the vpaths to includes that for some reason are now in the CFLAGS
+    #they are in the form -Dsomestuff-Isomepath -Ianotherpath ...
+    prev=0
+    tmpflg = ""
+    includes = list()
+    while (prev < len(flg)):
+        flgss = flg[prev:]  
+        #print("Old; " + flgss)      
+        
+        if flgss.startswith("-I"):
+            id1=0
+            flgss = " " + flgss
+        else:
+            id1 =flgss.find(" -I")
+               
+        if (id1 > -1):
+            
+            flgsst = flgss[id1+3:]
+            id2 =flgsst.find(" ")
+            
+            if (id2 == -1):
+                id2 = len(flgsst)
+            else:
+                id2 = id2
+            
+            flgsst = flgss[id1+3:id1+3+id2] #extract an include from the flg subsstring string
+            #print("Found: " + flgsst)
+            found = 0
+            iflgss = ""
+            for vpath in vpaths:                
+                if os.path.isdir(path.join(vpath, flgsst)) :
+                    iflgss = "-I" + path.join(vpath, flgsst) + ' '
+                    includes.append(path.join(vpath, flgsst))
+                    found = found +1                    
+                    break
+            if found == 0:                
+                iflgss = "ERROR: could not find: " + flgsst
+                print(iflgss)
+            if found > 1:
+                iflgss = "ERROR: found more than once: " + iflgss
+                print(iflgss)
+
+
+
+            #replace the original include from the flags
+            flg = flg[:prev+id1] + ' ' + iflgss +  flg[prev+id1+id2+3:]
+            #print("new: " + flg)
+            prev=prev+id1+len(iflgss) #skip to the next character after the upgraded include in the original string
+
+
+        else:
+            prev = len(flg)+1
+
+
     qflg = "" # append all quotes with escape char
     for i in range(0,len(flg)):
         if flg[i] == "\"":
@@ -39,6 +99,9 @@ for flg in cflags:
             qflg = qflg + flg[i]
     cmd = cmd + qflg + ' '
 
+
+#print(includes)
+#print("****************************************************")
 for i in range(0,len(srcs)):
     f = srcs[i]
     found = 0
@@ -48,10 +111,14 @@ for i in range(0,len(srcs)):
             found = found +1
             break
     if found == 0:
-        print("ERROR, could not find: " + f)
+        tmps = "ERROR, could not find: " + f
+        cmd = cmd + tmps #hack to make the error known
+        print(tmps)
     if found > 1:
-        print("ERROR, found more than once: " + f)
-
+        tmps = "ERROR, found more than once: " + f
+        cmd = cmd + tmps #hack to make the error known
+        print(tmps)
+#print(cmd)
 #call gcc
 os.system(cmd)
 
